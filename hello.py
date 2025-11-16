887658f552fbdb160879fdf536861adea0cf3b88
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2025-11-16 20:11:50 +1000

    Add two new routes for error handling

--- hello.py
+++ hello.py
@@ -12,3 +12,11 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.route(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
+
