3baf1c74be47c7ba8188d700dbbae90339a5d70a
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2025-11-17 07:38:54 +1000

    Fix incorrect decorator and changed to errorhandler

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ def user(name):
 def page_not_found(error):
     return render_template('404.html'), 404
 
-@app.route(500)
+@app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500.html'), 500
 
