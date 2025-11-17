71b845fbbce77865b7e0afcc285439330f1012dd
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2025-11-17 13:49:07 +1000

    Continue working on config file

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int()
