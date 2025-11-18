2a4e6e3b1d2abaedda3393fc3137cce11b6922d8
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2025-11-18 11:56:48 +1000

    Create the app factory

--- main/__init__.py
+++ main/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
 import requests
-import os
 
-app = Flask(__name__)
+# app = Flask(__name__)
 NASA_API_KEY = os.environ.get('NASA_API_KEY')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     return render_template('index.html')
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('error.html'), 404
+# @app.errorhandler(404)
+# def not_found(error):
+#     return render_template('error.html'), 404
+
+def create_app():
+    app = Flask(__name__)
+    return app
